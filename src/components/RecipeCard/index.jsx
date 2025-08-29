777e07466c61fe1448ede817f2f0c5f086cd1113
Keerthan Jakkaraju <keerthan14.codes@gmail.com>
2025-08-29 13:53:09 +0530

    Add GET /api/dishes route and fix package.json types corrected

---
+++ src/components/RecipeCard/index.jsx
@@ -0,0 +1,13 @@
+// src/components/RecipeCard/index.jsx
+import React from "react";
+
+const RecipeCard = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to the Recipe App 🎉</h1>
+      <p>You are successfully logged in!</p>
+    </div>
+  );
+};
+
+export default RecipeCard;
